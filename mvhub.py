fb6f4833cdbe1561e105508850a1f2620730d4d3
c-amos <corbin_amos@yahoo.com>
2020-10-17 18:53:18 -0500

    adjusting movehub file

--- mvhub.py
+++ mvhub.py
@@ -7,6 +7,25 @@ import numpy as np
 
 # read csv
 df = pd.read_csv('movehubqualityoflife.csv').set_index('City')
-print(df.head())
+#print(df.head())
 
+# reversing crime ratings for accuracy
+df['Safety Rating'] = 100 - df['Crime Rating']
+df = df.drop(columns='Crime Rating')
 
+# calculating end totals
+temp_df = df[['Safety Rating','Purchase Power','Health Care','Pollution','Quality of Life']]
+
+vars = []
+for row in range(0,len(df)):
+  var = 0
+  for col in temp_df:
+    obj = temp_df[col].iloc[row]
+    var += obj
+  vars.append(var)
+
+df.insert(len(df.columns),'Totals',vars)
+print(df)
+
+# adding end totals of scores
+# print(len(df.iloc[1]))
