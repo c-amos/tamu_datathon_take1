dfb9fde825b875962d6c398b204321926f183d8c
c-amos <62968359+c-amos@users.noreply.github.com>
2020-10-17 15:35:38 -0500

    Update file.py

--- file.py
+++ file.py
@@ -1,11 +1,14 @@
-print('Hello World!')
-# something else
-
-url = 'https://fragilestatesindex.org/wp-content/uploads/2020/05/fsi-2020.xlsx'
+# initial imports ------------------------------------------------------------------
 import pandas as pd
+import urllib.request
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+# section title -----------------------------------------------------------------------------
+url = 'https://fragilestatesindex.org/wp-content/uploads/2020/05/fsi-2020.xlsx'
+urllib.request.urlopen(url)
+# ^ should retrieve excel file directly; double check
 
 df = pd.read_excel(url)
 #show first five rows of the data
