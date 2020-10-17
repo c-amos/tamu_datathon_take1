d37db9de7acbc01ac9e0df6582988138ab5d4308
c-amos <62968359+c-amos@users.noreply.github.com>
2020-10-17 15:10:32 -0500

    Update file.py

--- file.py
+++ file.py
@@ -1,7 +1,7 @@
 print('Hello World!')
 # something else
 
-url = https://fragilestatesindex.org/wp-content/uploads/2020/05/fsi-2020.xlsx
+url = 'https://fragilestatesindex.org/wp-content/uploads/2020/05/fsi-2020.xlsx'
 import pandas as pd
 df = pd.read_excel(url)
 print(df)
