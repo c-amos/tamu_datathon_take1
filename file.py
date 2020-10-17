d327857a3c5b4f134d18d087b9e5b5e3bbe059da
c-amos <62968359+c-amos@users.noreply.github.com>
2020-10-17 15:38:38 -0500

    hopefully this retrieves the file directly

--- file.py
+++ file.py
@@ -7,7 +7,7 @@ import seaborn as sns
 
 # section title -----------------------------------------------------------------------------
 url = 'https://fragilestatesindex.org/wp-content/uploads/2020/05/fsi-2020.xlsx'
-urllib.request.urlopen(url)
+urllib.request.urlopen(url, headers={'User-agent' : 'Mozilla/5.0 (Windows; U; Windows NT 5.1; de; rv:1.9.1.5) Gecko/20091102 Firefox/3.5.5'})
 # ^ should retrieve excel file directly; double check
 
 df = pd.read_excel(url)
