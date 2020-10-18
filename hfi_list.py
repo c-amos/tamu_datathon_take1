e6da2e4d22d8977a2e41681fbfd1c678877dabe9
c-amos <corbin_amos@yahoo.com>
2020-10-17 20:19:17 -0500

    made the HFI csv less memory intensive

--- hfi_list.py
+++ hfi_list.py
@@ -4,6 +4,7 @@ import pandas as pd
 # actual work and effort ---------------------------------------------------------
 df = pd.read_csv('HFI.csv').sort_values('hf_score',ascending=False)
 
+df = df.loc[df['year'] == 2017]
 df = df[['year','countries','hf_score','hf_rank']]
 df = df.loc[df['hf_rank'] != '-']
 df.set_index('hf_rank')
