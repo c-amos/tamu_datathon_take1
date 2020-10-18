ff8411ff184620984baddf40dada3dc7078110b4
c-amos <corbin_amos@yahoo.com>
2020-10-17 20:12:00 -0500

    made the HFI csv less memory intensive

--- hfi_list.py
+++ hfi_list.py
@@ -4,14 +4,7 @@ import pandas as pd
 # actual work and effort ---------------------------------------------------------
 df = pd.read_csv('HFI.csv').drop(columns=['ISO_code','region']).sort_values('ef_rank')
 
-for i in range(0,len(df)):
-  if (df.iloc[i]['ef_rank'].isnumeric()): df['Drop?'] = False
-  else: df['Drop?'] = True
-  print('\r'+str(i) + ' out of ' + str(len(df)) + ' complete',end='')
+df = df[['year','countries','hf_score','hf_rank']]
+df = df.loc[df['hf_rank'] != '-']
 
-for i in range(0,len(df)):
-  if (df.iloc[i]['Drop?'] == True: df.drop
-  else: df['Drop?'] = True
-  print('\r'+str(i) + ' out of ' + str(len(df)) + ' complete',end='')
-
-print('\n',df)
+df.to_csv('HFI.csv')
